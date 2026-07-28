a0d35c5667a887b5d43576b9f0b4214cf176df2c
AhmadAzul <151420064+AhmadCpro@users.noreply.github.com>
2026-07-28 13:00:46 -0700

    Create pythonbot.py
    a very simple python bot for beginners in python you can play with the code around.

---
+++ pythonbot.py
@@ -0,0 +1,18 @@
+print("welcome, whats your name")
+response = input("")
+print("great, " + response + ("? you are welcome \
+how old are you? " + response))
+his_age = input()
+print(response + " you are " + his_age)
+print("Which country do you belong to")
+country_response = input("")
+print(country_response + " that's beautiful")
+print("you mind telling me your favorite \
+programming language? ")
+fav_language = input("")
+print("wow " + fav_language)
+print("what about your dream cybersecurity job")
+dream_job = input("")
+print("that's a good choice " + dream_job)
+for i in range(600,5000):
+	  print(random.range())
